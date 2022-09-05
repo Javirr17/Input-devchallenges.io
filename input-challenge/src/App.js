d0bc42d3b35cceb90d9575ac92fc1589f921b037
Javier Romero Rodriguez <javierybromero@gmail.com>
2022-09-05 13:59:51 +0200

    navbar and first input implemented

--- input-challenge/src/App.js
+++ input-challenge/src/App.js
@@ -1,9 +1,63 @@
 import './App.css';
+import Input from "./Input.jsx"
 
 function App() {
   return (
     <div className="App">
-      
+      <div className="left-navbar">
+        <p id="title"><span>Dev</span>challenges.io</p>
+        <div className="navbar-text-container">
+          <p className="navbar-text">Colors</p>
+          <p className="navbar-text">Typography</p>
+          <p className="navbar-text">Spaces</p>
+          <p className="navbar-text">Buttons</p>
+          <p className="navbar-text bold-text">Inputs</p>
+          <p className="navbar-text">Grid</p>
+        </div>
+      </div>
+      <div>
+        <div className="main-container">
+        <h3>Inputs</h3>
+    
+        <section className="button-layout">
+            <div className="button-div">
+              <p>{"<Input />"}</p>
+              <Input />
+            </div>
+            
+            <div className="button-div">
+              <p>{}</p>
+              
+            </div>
+
+            <div className="button-div">
+              <p>{}</p>
+              
+            </div>
+
+            <div className="button-div">
+              <p>{}</p>
+              
+            </div>
+
+            <div className="button-div">
+              <p>{}</p>
+              
+            </div>
+
+            <div className="button-div">
+              <p>{}</p>
+              
+            </div>
+
+        </section>
+
+        <footer>
+          <p>Icons: https://material.io/resources/icons/?style=round</p>
+          <p id="creator-msg">created by Javier Romero - devChallenges.io</p>
+        </footer>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ input-challenge/src/Input.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Input.css"
+
+function Input() {
+    return (
+        <div>
+            <p className="label">Label</p>
+            <input
+            placeholder="Placeholder">
+
+            </input>
+        </div>
+    )
+}
+
+export default Input;
